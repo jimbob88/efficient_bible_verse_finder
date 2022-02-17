cae2fbb5c4486de887532f8778ea5f9b32dd028c
James Blackburn <blackburnfjames@gmail.com>
2022-02-17 17:09:41 +0000

    Added error checking and bible classification

--- main.c
+++ main.c
@@ -4,6 +4,18 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int compareArrays(char a[], char b[], long unsigned int n)
+{
+    // Adapted from https://stackoverflow.com/a/24153920/5210078
+    int ii;
+    for (ii = 1; ii <= n; ii++)
+    {
+        if (a[ii] != b[ii])
+            return 0;
+    }
+    return 1;
+}
+
 int main(int argc, char *const argv[])
 {
     int c;
@@ -26,10 +38,35 @@ int main(int argc, char *const argv[])
     {
         printf("ERROR: -b OPTION IS REQUIRED\n");
         exit(1);
-    }
-    else
-    {
-        printf("%s\n", bible_name);
     };
+
+    printf("Checking bible exists: %s\n", bible_name);
+
+    printf("Performing search in ./bibles.txt\n");
+
+    FILE *filePointer;
+    int bufferLength = 30;
+    char buffer[bufferLength];
+
+    filePointer = fopen("bibles.txt", "r");
+
+    // check bible exists in bibles.txt
+    int bible_exists = 0;
+    while (fgets(buffer, bufferLength, filePointer))
+    {
+        if (compareArrays(buffer, bible_name, sizeof bible_name))
+        {
+            printf("Bible exists\n");
+            bible_exists = 1;
+        }
+    }
+    if (!bible_exists)
+    {
+        printf("ERROR: Bible does not exist\n");
+        exit(1);
+    }
+
+    fclose(filePointer);
+
     return 0;
 }
